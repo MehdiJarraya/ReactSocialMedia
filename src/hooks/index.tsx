dffaeda392f00f4d57012ed5417c3ee7cdca8f85
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-11-22 11:02:12 +0100

    uselocalStorage hook

---
+++ src/hooks/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export const useLocalStorage = (key: string) => {
+    // initialize the value from localStorage
+    const [currentValue, setCurrentValue] = useState<string | null>(() =>
+        localStorage.getItem(key)
+    );
+
+    // on every render, re-subscribe to the storage event
+    useEffect(() => {
+        const handler = (e: StorageEvent) => {
+            if (e.storageArea === localStorage && e.key === key) {
+                setCurrentValue(e.newValue);
+            }
+        };
+        window.addEventListener('storage', handler);
+        return () => {
+            window.removeEventListener('storage', handler);
+        };
+    });
+
+    // update localStorage when the currentValue changes via setCurrentValue
+    useEffect(() => {
+        if (currentValue) {
+            localStorage.setItem(key, currentValue);
+        }
+    }, [key, currentValue]);
+
+    // use as const to tell TypeScript this is a tuple
+    return [currentValue, setCurrentValue] as const;
+};
